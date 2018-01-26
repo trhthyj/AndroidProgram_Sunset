d56875d9ddafc2235f7323c8bbfcb449ceb6fecc
wangming <wangming@baokeyun.com>
2018-01-26 16:13:59 +0800

    32th chapter Animator

---
+++ app/src/main/java/com/mi/www/sunset/SunsetFragment.java
@@ -0,0 +1,91 @@
+package com.mi.www.sunset;
+
+
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationSet;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SunsetFragment extends Fragment {
+    private View mSceneView;
+    private ImageView mSunView;
+    private FrameLayout mSkyView;
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
+
+    public static SunsetFragment newInstance(){
+        return new SunsetFragment();
+    }
+
+    public SunsetFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+       mSceneView = view;
+       mSunView = view.findViewById(R.id.sun);
+       mSkyView = view.findViewById(R.id.sky);
+
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
+       mSceneView.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startAnimation();
+           }
+       });
+       return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3200);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());//加速
+
+        //天空颜色变化
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3200);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        //太阳落下之后天空颜色变为午夜蓝
+        ObjectAnimator nightSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                .setDuration(3200);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        /*heightAnimator.start();
+        sunsetSkyAnimator.start();*/
+
+        //多个动画播放时可以用AnimatorSet
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet
+                .play(heightAnimator)
+                .with(sunsetSkyAnimator)
+                .before(nightSkyAnimator);
+        animatorSet.start();
+    }
+
+}
